9135712911448fa4a505605ef8ff626929f22ecb
cabrelkemfang <ghislaincabrel.kemfang@gmail.com>
2018-10-29 18:08:24 +0100

    update

--- clients/family-members/add-family-members/add-family-members.component.ts
+++ clients/family-members/add-family-members/add-family-members.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
 import { ClientsService } from '../../clients.service';
 
 @Component({
@@ -16,64 +15,93 @@ export class AddFamilyMemberComponent implements OnInit {
 
   /** add family member entry form. */
   addFamilyMember: FormGroup;
-  /**Is Dependent data */
-  dependentValues = ['True', 'False'];
-  /** Marital Status data */
-  maritalStatusData = ['Married', 'Single', 'Divorsed', 'Widowed'];
-  /** Gender data */
-  genderData = ['Male', 'Female', 'Mujer', 'Unspecified', 'Irlandes'];
-  
-  
-  
-  constructor(private formBuilder: FormBuilder,
-              private clientService: ClientsService, 
-              private route: ActivatedRoute, 
-              private router: Router
-              
+  genderIdOptionsData: any;
+  maritalStatusIdOptionsData: any;
+  professionIdOptionsData: any;
+  relationshipIdOptionsData: any;
+  disableSelect = new FormControl(false);
+  id: number = Number(this.route.snapshot.paramMap.get('id'));
 
-  ) { }
+
+
+  constructor(private formBuilder: FormBuilder,
+    private clientService: ClientsService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-   this.createAddFamilyMemberForm();
+    this.createAddFamilyMemberForm();
+    this.getTemplate();
   }
+
+  getTemplate() {
+    this.clientService.getClientTemplate(this.id).subscribe(
+      (data: {
+        genderIdOptions: any;
+        maritalStatusIdOptions: any;
+        professionIdOptions: any;
+        relationshipIdOptions: any;
+      }) => {
+        this.genderIdOptionsData = data.genderIdOptions;
+        this.maritalStatusIdOptionsData = data.maritalStatusIdOptions;
+        this.professionIdOptionsData = data.professionIdOptions;
+        this.relationshipIdOptionsData = data.relationshipIdOptions;
+      });
+  }
+
+
 
   /**
    * Creates the add family member form.
    */
   createAddFamilyMemberForm() {
     this.addFamilyMember = this.formBuilder.group({
-      'firstname': [''],
-      'middlename': [''],
-      'lastname': [''],
+      'clientId': [''],
+      'firstName': [''],
+      'middleName': [''],
+      'lastName': [''],
       'qualification': [''],
-      'mobilenumber': [''],
+      'mobileNumber': [''],
       'age': [''],
-      'dependent': [''],
-      'relationship': [''],
-      'maritalStatus': [''],
-      'Gender': [''],
-      'profession': [''],
-      'dob': ['']
+      'isDependent': [''],
+      'relationshipId': [''],
+      'maritalStatusId': [''],
+      'genderId': [''],
+      'professionId': [''],
+      'dateOfBirth': ['']
     });
   }
 
-   /**
+  /**
    * Submits the add family member form and creates a family member,
    * if successful redirects to family member page.
    */
-    submit() {
-      const familyMember = this.addFamilyMember.value;
-      familyMember.locale = 'en';
-      familyMember.dateFormat = 'yyyy-MM-dd';
-      const id = this.route.snapshot.paramMap.get('id');
-      console.log(familyMember);
-
-      this.clientService.createFamilyMember(familyMember, id);//.subscribe(response => {
-       // this.router.navigate(['../../family-members', response.clientId], { relativeTo: this.route }); 
-     // });
-   
+  submit() {
+    const familyMember = this.addFamilyMember.value;
+    familyMember.locale = 'en';
+    familyMember.dateFormat = 'yyyy-MM-dd';
+    if (familyMember.dateOfBirth instanceof Date) {
+      let day = familyMember.dateOfBirth.getDate();
+      let month = familyMember.dateOfBirth.getMonth() + 1;
+      const year = familyMember.dateOfBirth.getFullYear();
+      if (day < 10) {
+        day = `0${day}`;
+      }
+      if (month < 10) {
+        month = `0${month}`;
+      }
+      familyMember.dateOfBirth = `${year}-${month}-${day}`;
     }
-      
+
+    familyMember.isDependent = this.disableSelect.value;
+
+    familyMember.clientId = this.id;
+    console.log(familyMember);
+
+    this.clientService.createFamilyMember(familyMember, this.id).subscribe(response => {
+      this.router.navigate(['../../family-members', response.clientId], { relativeTo: this.route });
+    });
+  }
 }
- 
+
 
